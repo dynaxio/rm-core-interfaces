803eab939a90cc4586f23e8a6e38e524abdb5ce7
aherzog <a.herzog@dynax.io>
2023-02-03 10:42:18 +0100

    Base Structure

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * config.php
+ *
+ * rmCore Interfaces Main Config
+ *
+ * @category   Core
+ * @package    rm-core-interfaces
+ * @author     Andreas
+ * @copyright  2023 RM Group AG
+ * @version    1.0.0
+ */
+
+/*
+ * Directories & URLS
+ */
+define('INTERFACES_DIR_TMP',        '/tmp');
+
+
+?>
---
+++ sourcetax/import-sourcetax.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * config.php
+ *
+ * rmCore Interfaces Import: Source Tax
+ *
+ * @category   Interface/Import
+ * @package    rm-core-interfaces
+ * @author     Andreas
+ * @copyright  2023 RM Group AG
+ * @version    0.0.1
+ */
+
+
+
+?>
